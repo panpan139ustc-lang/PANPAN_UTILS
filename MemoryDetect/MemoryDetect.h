b3b0fe6dae29a62cb878213c655bb9cb2c14472e
panpan139ustc-lang <panpan139ustc@gmail.com>
2025-11-29 18:37:51 +0800

    Add files via upload

---
+++ MemoryDetect/MemoryDetect.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+
+
+void* operator new(std::size_t size, const char* file, int line);
+void* operator new[](std::size_t size, const char* file, int line);
+void* operator new(std::size_t size);
+void* operator new(std::size_t size);
+void operator delete(void* ptr)noexcept;
+void operator delete[](void* ptr)noexcept;
+
+#define DEBUG_NEW new(__FILE__, __LINE__)
+
+//#define new new(__FILE__, __LINE__);
+
+int checkLeaks();
+
+extern bool new_autocheck_flag;
+extern bool new_verbose_flag;
---
+++ MemoryDetect/test.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <memory>
+#include <mutex>
+
+#define ENABLE_DEBUG_NEW
+#include "MemoryDetect.h"
+
+#ifdef ENABLE_DEBUG_NEW
+#  define new DEBUG_NEW
+#endif
+
+struct A {
+	int a;
+};
+
+int main() {
+	printf("memory detect\n");
+
+	new_autocheck_flag = true;
+	//new_verbose_flag = true;
+	int* ptr1 = new int;
+	delete ptr1;
+
+	int* ptr2 = new int[4];
+	delete[] ptr2;
+
+	A* a1 = new A;
+	//delete a1
+	A* a2 = new A;
+	delete a2;
+
+	{ std::shared_ptr<A> a = std::make_shared<A>(); }
+
+	checkLeaks();
+
+	return 0;
+
+
+
+}
